e97674456a0769891a33d6016266e493a47f8bd3
hellowein <hellowein2@gmail.com>
2025-01-14 22:44:09 +0300

    update

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, f'Ку {message.from_user.first_name} я todo list {__version__}')
+    kb = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='Добавить задачу', callback_data=f'add_task')
+    btn2 = types.InlineKeyboardButton(text='Просмотреть задачи', callback_data='view_tasks')
+    kb.add(btn1, btn2)
+
+    bot.send_message(message.chat.id, f'Ку {message.from_user.first_name} я todo list {__version__}', reply_markup=kb)
+
+
+@bot.message_handler(content_types=['text'])
+def chatting(message):
+    bot.delete_message(message.chat.id, message.message_id)
 
 
 bot.infinity_polling()
