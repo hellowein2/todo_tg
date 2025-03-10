a11b7b8820f7bf2bf4df1a3e538d236016c28f68
hellowein <hellowein2@gmail.com>
2025-03-11 01:13:13 +0300

    update

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo apt-get update && sudo apt-get upgrade
 docker build -t my-python-app .
 echo "Введите токен бота"
 read YOUR_API_TOKEN
-docker run -v /db:/app/ignore -d -e TZ=Europe/Moscow --name telegram_bot -e BOT_TOKEN='$YOUR_API_TOKEN' my-python-app
+docker run -v /db:/app/ignore -d -e TZ=Europe/Moscow --name telegram_bot -e BOT_TOKEN="$YOUR_API_TOKEN" my-python-app
