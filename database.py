241a59499ffcd5a466825f9b26ced64122aa9454
hellowein <hellowein2@gmail.com>
2025-03-14 16:47:30 +0300

    refactoring

---
+++ database.py
@@ -0,0 +1,77 @@
+import sqlite3
+from datetime import datetime
+
+
+class Database:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self._create_tables()
+
+    def _create_tables(self):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS Users (
+                        user_id INTEGER PRIMARY KEY,
+                        username TEXT
+                    )
+                ''')
+
+    def add_user(self, user_id, username):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS Users (
+                        user_id INTEGER PRIMARY KEY,
+                        username TEXT
+                    )
+                ''')
+            cursor.execute('INSERT OR REPLACE INTO Users (user_id, username) VALUES (?, ?)'
+                           , (user_id, username))
+
+            cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS Tasks{user_id} (
+            task TEXT NOT NULL,
+            time TEXT NOT NULL,
+            done INTEGER NOT NULL DEFAULT 0
+            )
+            ''')
+
+    def create_tak(self, user_id, task, time):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+
+            cursor.execute(f'INSERT INTO Tasks{user_id} (task, time) VALUES (?, ?)',
+                           (f'{task}', f'{time}'))
+
+    def get_tasks(self, user_id):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+
+            pending_tasks = cursor.execute(f'SELECT rowid, * FROM Tasks{user_id} WHERE done = ?',
+                                           (0,)).fetchall()
+            completed_tasks = cursor.execute(f'SELECT rowid, * FROM Tasks{user_id} WHERE done = ?',
+                                             (1,)).fetchall()
+
+            return pending_tasks, completed_tasks
+
+    def get_selected_task(self, user_id, task_id):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(f'SELECT * FROM Tasks{user_id} WHERE rowid = ?', (task_id,))
+            task = cursor.fetchone()
+            return task
+
+    def delete_task(self, user_id, task_id):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(f'DELETE FROM Tasks{user_id} WHERE rowid = ?', (task_id,))
+
+    def done_task(self, user_id, task_id):
+        with sqlite3.connect('ignore/data.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(f'UPDATE Tasks{user_id} SET done = ? WHERE rowid = ?', (1, task_id))
+            cursor.execute(f'UPDATE Tasks{user_id} SET time = ? WHERE rowid = ?',
+                           (f'{datetime.today().strftime("%d.%m.%Y %H:%M")}', task_id))
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telebot
-import sqlite3
 from datetime import datetime
 from telebot import types
 import locale
 import platform
 from babel.dates import format_date
 import os
+
+from database import Database
+
+db = Database('ignore/data.db')
 
 API_TOKEN = os.environ.get('BOT_TOKEN')
 
@@ -17,26 +20,7 @@ locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 def add_users(message):
     user_id = message.from_user.id
     username = message.from_user.username
-
-    with sqlite3.connect('ignore/data.db') as connection:
-        cursor = connection.cursor()
-
-        cursor.execute('''
-                CREATE TABLE IF NOT EXISTS Users (
-                    user_id INTEGER PRIMARY KEY,
-                    username TEXT
-                )
-            ''')
-        cursor.execute('INSERT OR REPLACE INTO Users (user_id, username) VALUES (?, ?)'
-                       , (user_id, username))
-
-        cursor.execute(f'''
-        CREATE TABLE IF NOT EXISTS Tasks{user_id} (
-        task TEXT NOT NULL,
-        time TEXT NOT NULL,
-        done INTEGER NOT NULL DEFAULT 0
-        )
-        ''')
+    db.add_user(user_id, username)
 
 
 def add_task(message, edit_msg):
@@ -45,16 +29,16 @@ def add_task(message, edit_msg):
     btn2 = types.InlineKeyboardButton(text='Просмотреть задачи', callback_data='view_tasks')
     kb.add(btn1, btn2)
 
-    with sqlite3.connect('ignore/data.db') as connection:
-        cursor = connection.cursor()
+    user_id = message.from_user.id
+    task = message.text
+    time = datetime.today().strftime("%d.%m.%Y %H:%M")
 
-        cursor.execute(f'INSERT INTO Tasks{message.from_user.id} (task, time) VALUES (?, ?)',
-                       (f'{message.text}', f'{datetime.today().strftime("%d.%m.%Y %H:%M")}'))
-
-    bot.edit_message_text(chat_id=message.from_user.id, text=f'Вы добавили задачу: {message.text}',
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.edit_message_text(chat_id=message.from_user.id, text=f'Вы добавили задачу: {task}',
                           message_id=edit_msg.message_id,
                           reply_markup=kb)
-    bot.delete_message(message.chat.id, message.message_id)
+
+    db.create_tak(user_id, task, time)
 
 
 def format_date_russian(date_string):
@@ -110,35 +94,31 @@ def pre_add_task(call):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'view_tasks')
 def view_tasks(call):
-    with sqlite3.connect('ignore/data.db') as connection:
-        cursor = connection.cursor()
-        kb = types.InlineKeyboardMarkup()
+    user_id = call.message.chat.id
+    pending_tasks, completed_tasks = db.get_tasks(user_id)
 
-        pending_tasks = cursor.execute(f'SELECT rowid, * FROM Tasks{call.message.chat.id} WHERE done = ?',
-                                       (0,)).fetchall()
-        completed_tasks = cursor.execute(f'SELECT rowid, * FROM Tasks{call.message.chat.id} WHERE done = ?',
-                                         (1,)).fetchall()
+    kb = types.InlineKeyboardMarkup()
 
-        if pending_tasks or completed_tasks:
-            for i in pending_tasks:
-                btn = types.InlineKeyboardButton(text=i[1], callback_data=f'task_id_{i[0]}')
+    if pending_tasks or completed_tasks:
+        for i in pending_tasks:
+            btn = types.InlineKeyboardButton(text=i[1], callback_data=f'task_id_{i[0]}')
+            kb.add(btn)
+        if completed_tasks:
+            for i in completed_tasks:
+                btn = types.InlineKeyboardButton(text=f'{i[1]} ✅', callback_data=f'task_id_{i[0]}')
                 kb.add(btn)
-            if completed_tasks:
-                for i in completed_tasks:
-                    btn = types.InlineKeyboardButton(text=f'{i[1]} ✅', callback_data=f'task_id_{i[0]}')
-                    kb.add(btn)
 
-            btn = types.InlineKeyboardButton('Назад', callback_data='back')
-            kb.add(btn)
-            bot.edit_message_text(chat_id=call.message.chat.id,
-                                  message_id=call.message.message_id,
-                                  text='Ваши задачи:', reply_markup=kb)
-        else:
-            btn = types.InlineKeyboardButton(text='Добавить задачу', callback_data='add_task')
-            kb.add(btn)
-            bot.edit_message_text(chat_id=call.message.chat.id,
-                                  message_id=call.message.message_id,
-                                  text='Задач ещё нет', reply_markup=kb)
+        btn = types.InlineKeyboardButton('Назад', callback_data='back')
+        kb.add(btn)
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              text='Ваши задачи:', reply_markup=kb)
+    else:
+        btn = types.InlineKeyboardButton(text='Добавить задачу', callback_data='add_task')
+        kb.add(btn)
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              text='Задач ещё нет', reply_markup=kb)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -148,58 +128,41 @@ def callback_handler(call):
         handle_selected_task(call, task_id)
     elif call.data.startswith('delete_task_'):
         task_id = call.data.split('_')[2]
-        delete_selected_task(call, task_id)
+        db.delete_task(call.message.chat.id, task_id)
+        view_tasks(call)
     elif call.data.startswith('done_task_'):
         task_id = call.data.split('_')[2]
-        done_task(call, task_id)
+        db.done_task(call.message.chat.id, task_id)
+        view_tasks(call)
     elif call.data == 'back':
         back_to_main(call)
 
 
 def handle_selected_task(call, task_id):
-    with sqlite3.connect('ignore/data.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(f'SELECT * FROM Tasks{call.message.chat.id} WHERE rowid = ?', (task_id,))
-        task = cursor.fetchone()
+    task = db.get_selected_task(call.message.chat.id, task_id)
 
-        formatted_date = format_date_russian(task[1])
+    formatted_date = format_date_russian(task[1])
 
-        kb = types.InlineKeyboardMarkup()
-        btn3 = types.InlineKeyboardButton('Назад', callback_data='view_tasks')
+    kb = types.InlineKeyboardMarkup()
+    btn3 = types.InlineKeyboardButton('Назад', callback_data='view_tasks')
 
-        if task[2] == 0:
-            btn1 = types.InlineKeyboardButton('Вычеркнуть', callback_data=f'done_task_{task_id}')
-            btn2 = types.InlineKeyboardButton('Удалить', callback_data=f'delete_task_{task_id}')
-            kb.add(btn1, btn2)
-            kb.add(btn3)
+    if task[2] == 0:
+        btn1 = types.InlineKeyboardButton('Вычеркнуть', callback_data=f'done_task_{task_id}')
+        btn2 = types.InlineKeyboardButton('Удалить', callback_data=f'delete_task_{task_id}')
+        kb.add(btn1, btn2)
+        kb.add(btn3)
 
-            bot.edit_message_text(chat_id=call.message.chat.id,
-                                  message_id=call.message.message_id,
-                                  text=f'{task[0]} - созданно {formatted_date}', reply_markup=kb)
-        else:
-            btn = types.InlineKeyboardButton('Удалить', callback_data=f'delete_task_{task_id}')
-            kb.add(btn)
-            kb.add(btn3)
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              text=f'{task[0]} - созданно {formatted_date}', reply_markup=kb)
+    else:
+        btn = types.InlineKeyboardButton('Удалить', callback_data=f'delete_task_{task_id}')
+        kb.add(btn)
+        kb.add(btn3)
 
-            bot.edit_message_text(chat_id=call.message.chat.id,
-                                  message_id=call.message.message_id,
-                                  text=f'{task[0]} - сделанно {formatted_date}', reply_markup=kb)
-
-
-def delete_selected_task(call, task_id):
-    with sqlite3.connect('ignore/data.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(f'DELETE FROM Tasks{call.message.chat.id} WHERE rowid = ?', (task_id,))
-    view_tasks(call)
-
-
-def done_task(call, task_id):
-    with sqlite3.connect('ignore/data.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(f'UPDATE Tasks{call.message.chat.id} SET done = ? WHERE rowid = ?', (1, task_id))
-        cursor.execute(f'UPDATE Tasks{call.message.chat.id} SET time = ? WHERE rowid = ?',
-                       (f'{datetime.today().strftime("%d.%m.%Y %H:%M")}', task_id))
-    view_tasks(call)
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              text=f'{task[0]} - сделанно {formatted_date}', reply_markup=kb)
 
 
 if __name__ == '__main__':
