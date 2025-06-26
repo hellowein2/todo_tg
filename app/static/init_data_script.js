7913eae24837020c48caa014f2b74c46bf5c14cf
pc in honor of Ross <hellowein2@gmail.com>
2025-06-27 00:06:45 +0300

    grgdgr

--- app/static/init_data_script.js
+++ app/static/init_data_script.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return null;
+    }
+
     if (window.Telegram && window.Telegram.WebApp) {
-        // Инициализация Web App
         window.Telegram.WebApp.ready();
 
-        // Получаем initData (строка параметров)
-        let initData = window.Telegram.WebApp.initData;
+        if (!getCookie('user_cookie')) { // Запускаем только если куки нет
+            let initData = window.Telegram.WebApp.initData;
 
-        // Отправляем initData на бек
-        fetch('https://serega-sosi.ru/verify', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ initData: initData }),
-            credentials: 'include'
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Ответ сервера:', data);
-            window.location.href = "/";
-        }).catch(error => {
-            console.error('Ошибка:', error);
-        });
+            fetch('https://serega-sosi.ru/verify', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ initData: initData }),
+                credentials: 'include'
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Ответ сервера:', data);
+                window.location.href = "/";
+            }).catch(error => {
+                console.error('Ошибка:', error);
+            });
+        } else {
+            console.log('Кука user_cookie уже есть, запрос не нужен');
+        }
     } else {
         console.error("Telegram WebApp не найден");
     }
