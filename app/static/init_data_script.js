e46e1db018771729208cf2f46c05dd4b3a4124a5
pc in honor of Ross <hellowein2@gmail.com>
2025-06-26 23:40:43 +0300

    grgdgr

--- app/static/init_data_script.js
+++ app/static/init_data_script.js
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => {
             console.log('Ответ сервера:', data);
-        }).then(window.location.reload)
-        .catch(error => {
+        }).then(() => window.location.reload())
+            .catch(error => {
             console.error('Ошибка:', error);
         });
     } else {
