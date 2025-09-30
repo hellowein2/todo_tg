58d16d3e764d1a9f075058ecac98dfe3af107711
pc in honor of Ross <hellowein2@gmail.com>
2025-09-30 16:18:37 +0300

    update

--- main_bot/main_bot.py
+++ main_bot/main_bot.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.send_message(chat_id=message.chat.id, text='333')
+    bot.send_message(chat_id=message.chat.id, text='444')
 
 
 if __name__ == '__main__':
