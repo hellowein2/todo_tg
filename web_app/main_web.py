a19df12dd253b08d2d878b257174eb05688ea3fb
pc in honor of Ross <hellowein2@gmail.com>
2025-10-01 19:25:50 +0300

    update

--- web_app/main_web.py
+++ web_app/main_web.py
@@ -55,15 +55,13 @@ async def delete_task(task_name: str):
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request, user_cookie: Annotated[int | None, Cookie()]= None):
-    if user_cookie is None:
-        return RedirectResponse("/verify")
-    else:
-        p, c = db.get_tasks(user_cookie)
-        pending_tasks = [f"{i[1]}" for i in p]
-        completed_tasks = [f"{i[1]}" for i in c]
-        return templates.TemplateResponse('index.html', {'request': request,
-                                                         'pending_tasks': pending_tasks,
-                                                         'completed_tasks': completed_tasks})
+
+    p, c = db.get_tasks(user_cookie)
+    pending_tasks = [f"{i[1]}" for i in p]
+    completed_tasks = [f"{i[1]}" for i in c]
+    return templates.TemplateResponse('index.html', {'request': request,
+                                                     'pending_tasks': pending_tasks,
+                                                     'completed_tasks': completed_tasks})
 
 
 async def validate_telegram_init_data(init_data: str):
